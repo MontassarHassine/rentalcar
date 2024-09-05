302b0c36a6af81bb7326a84b99f8a74ae1e4fba9
MontassarHassine <montasar.hassine@gmail.com>
2024-09-05 13:30:03 +0100

    admin access to dashbord

---
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string'
+        ]) ;
+
+        $check = Auth::guard('admin')->attempt($data);
+        $admin = Auth::guard('admin')->user();
+       Auth::login($admin);
+      return redirect()->route('dashboard') ;
+    }
+}
--- database/migrations/2024_09_05_111349_create_admins_table.php
+++ database/migrations/2024_09_05_111349_create_admins_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('user_name');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
         });
